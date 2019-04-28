da5bcdc609b922c41752e8722788ea8550677d8a
Nikitas Daniil <daniil_nikitas@mail.ru>
2019-04-28 23:56:44 +0600

    creating a row in database with vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
 <div id="app">
   <ul id="list" v-if="car_info_set.length">
-    <li v-for="(element, key) in car_info_set" :key="key">
+    <li v-for="(element, key) in car_info_set" :key="key" class="car_item">
       brand is <b>{{element.brand}}</b>
       model is <b>{{element.model}}</b>
+      engine is <b>{{element.engine}}</b>
+      gear box is <b>{{element.gear_box}}</b>
       <button class="btn btn-danger" id="delete_item" @click="delete_item(element.car_id)">Delete</button>
       <button class="btn btn-primary" id="edit_item">Edit</button>
     </li>
   </ul>
   <div v-else class="err_list"> {{err_msg}} </div>
-  <img :src="src" alt="">
+  <hr>
+  <ul id="create_item">
+    <li>
+      <span>Brand: </span>
+      <input placeholder="please enter brand" class="form-control" type="text" v-model.trim="newItem.brand">
+    </li>
+    <li>
+      <span>Model: </span>
+      <input placeholder="please enter model" class="form-control" type="text" v-model.trim="newItem.model">
+    </li>
+    <li>
+      <span>Engine: </span>
+      <select class="form-control" name="" id="" v-model="newItem.engine">
+        <option value="1">Petrol</option>
+        <option value="2">Diesel</option>
+        <option value="3">Electric</option>
+        <option value="4">Hybrid</option>
+        <option value="5">Hydrogen</option>
+      </select >
+    </li>
+    <li>
+      <span>Automatic: </span>
+      <input class="form-control" type="radio" name="" value="1" v-model="newItem.gearbox">
+      <span>Manual: </span>
+      <input class="form-control" type="radio" name="" value="2" v-model="newItem.gearbox">
+    </li>
+    <li>
+      <button class="btn btn-dark" :disabled="btn_switch" @click="create_item">Create</button>
+    </li>
+  </ul>
 </div>
 
 </template>
@@ -20,13 +51,27 @@ import $ from 'jquery'
 export default {
   data () {
     return {
+      newItem: {
+        brand: '',
+        model: '',
+        engine: '3',
+        gearbox: '1'
+      },
       car_info_set: [],
       err_msg: 'sorry, no car in record',
       src: require('./logo.png'),
       url: 'http://php-server/server.php'
     }
   },
-  computed: {},
+  computed: {
+    btn_switch () {
+      if (this.newItem.brand.length >= 1 && this.newItem.model.length >= 1) {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
   components: {},
   created () {
     this.retrieve_all()
@@ -46,6 +91,7 @@ export default {
       })
         .done((data) => {
           let result = JSON.parse(data)
+          console.log(result)
           if (result[0]) {
             that.car_info_set = result[1]
           } else {
@@ -80,19 +126,49 @@ export default {
         .fail((data) => {
           that.err_msg = data.statusText
         })
+    },
+    create_item () {
+      let that = this
+      $.post({
+        url: that.url,
+        data: {
+          action: 'create_item',
+          new_item: that.newItem
+        }
+      })
+        .always(() => {
+          that.car_info_set = []
+          that.err_msg = ''
+        })
+        .done((data) => {
+          let result = JSON.parse(data)
+          if (result[0]) {
+            that.retrieve_all()
+          } else {
+            that.err_msg = result[1]
+          }
+        })
+        .fail((data) => {
+          that.err_msg = data.statusText
+        })
     }
   }
 }
 </script>
 
 <style scoped>
+  #create_item {
+    background-color: deepskyblue;
+    padding: 10px;
+    color: white;
+  }
   .err_list {
     background-color: red;
     color: white;
     padding: 20px ;
     font-size: 20px;
   }
-  li {
+  .car_item {
     background-color: green;
     color: white;
     display: flex;
--- src/server.php
+++ src/server.php
@@ -8,7 +8,7 @@ if(!empty($_POST['action'])){
     }
     if($_POST['action']=='retrieve_all'){
 
-        $sql = "SELECT `car_id`, `brand`, `model` FROM `cars`";
+        $sql = "SELECT `car_id`, `brand`, `model`, `engine`, `gear_box` FROM `cars`";
         $result = $db->query($sql);
         if($result){
             if($result->num_rows==0){
@@ -38,6 +38,63 @@ if(!empty($_POST['action'])){
             }
         } else {
             echo json_encode([false,'need car id to delete...']);
+        }
+        $db->close();
+    } else if($_POST['action']=='create_item'){
+        // sanitizing data first
+        //echo json_encode([true,'Пришло']);
+        if(!empty($_POST['new_item'])){
+            if(!empty($_POST['new_item']['brand'])&&!empty($_POST['new_item']['model'])&&!empty($_POST['new_item']['engine'])&&!empty($_POST['new_item']['gearbox'])){
+                // check brand, model, engine and gearbox
+                // translate engine and gearbox code...
+                $brand = $db->real_escape_string($_POST['new_item']['brand']);
+                $model = $db->real_escape_string($_POST['new_item']['model']);
+                $engine = $db->real_escape_string($_POST['new_item']['engine']);
+                $gearbox = $db->real_escape_string($_POST['new_item']['gearbox']);
+
+                switch($engine){
+                    case 1:
+                        $engine = "Petrol";
+                        break;
+                    case 2:
+                        $engine = "Diesel";
+                        break;
+                    case 3:
+                        $engine = "Electric";
+                        break;
+                    case 4:
+                        $engine = "Hybrid";
+                        break;
+                    case 5:
+                        $engine = "Hydrigen";
+                        break;
+                    default:
+                        $engine = false;
+                }
+                if($gearbox ==1){
+                    $gearbox = "Automatic";
+                }elseif($gearbox ==2){
+                    $gearbox = "Manual";
+                }else{
+                    $gearbox = false;
+                }
+                if($engine==false||$gearbox == false){
+                    echo json_encode([false,'illegal data...']);
+                }else{
+                    $sql = "INSERT INTO `cars` SET `brand`='{$brand}', `model`='{$model}', `engine`='{$engine}', `gear_box`='{$gearbox}' ";
+                    $result = $db->query($sql);
+                    if($result){
+                        echo json_encode([true,'new row created']);
+                    }else{
+                        echo json_encode([false,'SQL Error']);
+                    }
+                }
+
+            }else{
+                echo json_encode([false,'insufficient data, cannot create new row...']);
+            }
+        }else{
+            echo json_encode([false,'no data sent, cannot create new row...']);
         }
         $db->close();
     }
