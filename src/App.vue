a62188edefce81df0655e7bb6cf6d2a4e338b6ee
Nikitas Daniil <daniil_nikitas@mail.ru>
2019-05-07 20:25:12 +0600

    using post and get with axios to PHP

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,51 @@
 <div id="app">
   <input type="text" v-model="text">
   <h2 :class="red">{{text}}</h2>
+  <button @click="trigger_get()">Axios GET</button>
+  <button @click="trigger_post()">Axios POST</button>
 </div>
 
 </template>
 
 <script>
+import axios from 'axios'
+import qs from 'qs'
 
 export default {
   data () {
     return {
       text: 'some text',
-      red: 'red'
+      red: 'red',
+      url: '/api/php-server/server.php'
     }
   },
   computed: {
   },
   methods: {
+    trigger_post () {
+      axios.post(this.url, qs.stringify({
+        msg: 'sent by POST method'
+      }))
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    trigger_get () {
+      axios.get(this.url, {
+        params: {
+          msg: 'HELLO from AXIOS GET'
+        }
+      })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   },
   components: {
   }
---
+++ src/server/server.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
+echo "this is PHP api".$_GET['msg'];
+//$request_body = file_get_contents("php://input");
+//$data = json_decode($request_body, true);
+//echo $data['msg'];
+echo $_POST['msg'];
+?>
